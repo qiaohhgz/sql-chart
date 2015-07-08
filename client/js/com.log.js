18e79a8ae7f38122bf0b2e1569e97696a4006c2b
乔仕举 <qiaohhgz@163.com>
2015-07-08 18:25:39 +0800

    时间补零

--- client/js/com.log.js
+++ client/js/com.log.js
@@ -1,6 +1,6 @@
 function log(v) {
     console.log(v);
-    if(typeof v == 'object'){
+    if (typeof v == 'object') {
         v = JSON.stringify(v);
     }
     if ($("#log").children().length >= 20) {
@@ -8,7 +8,11 @@ function log(v) {
     }
     $("#log").append("<p>" + v + "</p>");
 }
-function dateToString(val){
+function dateToString(val) {
     var d = new Date(parseInt(val));
-    return d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
+    var h = d.getHours(), m = d.getMinutes(), s = d.getSeconds();
+    if (h < 10) h = "0" + h;
+    if (m < 10) m = "0" + m;
+    if (s < 10) s = "0" + s;
+    return h + ":" + m + ":" + s;
 }
